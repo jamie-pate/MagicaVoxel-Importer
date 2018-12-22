bab729c262c6e1d2f028a08e7e90751a72fd7185
Jamie Pate <jpate@fortinet.com>
2018-12-21 17:17:23 -0800

    Add bones support so we can animate these things.

--- addons/MagicaVoxelImporter/plugin.gd
+++ addons/MagicaVoxelImporter/plugin.gd
@@ -18,55 +18,130 @@ func _exit_tree():
 ##############################################
 #                Import Plugin               #
 ##############################################
-class MagicaVoxelData:
+
+class MV:
+	# revive a string from the stream
+	func mv_str(file):
+		var size = file.get_32()
+		var buff = file.get_buffer(size)
+		# assume utf8? not sure, but should be safe
+		return buff.get_string_from_utf8()
+
+	# revive a dict from the stream
+	func mv_dict(file, unpack):
+		var size = file.get_32()
+		var result = {}
+		for i in size:
+			var key = mv_str(file)
+			assert(key in unpack)
+			match unpack[key]:
+				's':
+					result[key] = mv_str(file)
+				'int8':
+					result[key] = file.get_8()
+				'int32':
+					result[key] = file.get_32()
+				'int32x3':
+					result[key] = Vector3(file.get_8(), file.get_8(), file.get_8())
+				_:
+					print('Couldn\'t unpack key %s' % key)
+					assert(false)
+		return result
+
+class MVChunk:
+	var id
+	var size
+	var child_count
+	var position
+	var header_position
+
+	func init(file):
+		header_position = file.get_position()
+		id = PoolByteArray([file.get_8(),file.get_8(),file.get_8(),file.get_8()]).get_string_from_ascii() #char[] chunkId
+		size = file.get_32()
+		child_count = file.get_32()
+		position = file.get_position()
+		#print('id:%s sz:%d cc:%d' % [id, size, child_count])
+
+class MVVoxels:
 	var pos = Vector3(0,0,0)
 	var color
+	# contain which chunk we are in so we can find it's name later?
+	var chunkNum = -1
+	var bone
 	func init(file):
 		pos.x = file.get_8()
 		pos.z = -file.get_8()
 		pos.y = file.get_8()
-		
+
 		color = file.get_8()
+
+class MVTransformNode extends MV:
+	var node_id
+	var attr
+	var child_node_id
+	var reserved_id
+	var layer_id
+	var frame_count
+	var frames
+
+	func init(file):
+		node_id = file.get_32()
+		attr = mv_dict(file, {
+			'_name': 's',
+			# NOTE: not sure if this conversion for `_hidden`
+			# is correct since the docs just say 1/0
+			# '_hidden': 'int32',
+		})
+		child_node_id = file.get_32()
+		reserved_id = file.get_32()
+		layer_id = file.get_32()
+		frame_count = file.get_32()
+		frames = []
+		assert(frame_count == 1)
+		for i in frame_count:
+			frames.append(mv_dict(file, {'_r': 'int8', '_t': 'int32x3'}))
 
 class ImportPlugin extends EditorImportPlugin:
 	#The Name shown in the Plugin Menu
 	func get_importer_name():
 		return 'MagicaVoxel-Importer'
-	
+
 	#The Name shown under 'Import As' in the Import menu
 	func get_visible_name():
 		return "MagicaVoxels as Points"
-	
+
 	#The File extensions that this Plugin can import. Those will then show up in the Filesystem
 	func get_recognized_extensions():
 		return ['vox']
-	
+
 	#The Resource Type it creates. Im still not sure what exactly this does
 	func get_resource_type():
 		return "Mesh"
-	
+
 	#The extenison the imported file will have
 	func get_save_extension():
 		return 'mesh'
-	
+
 	#Returns an Array or Dictionaries that declare which options exist.
 	#Those options will show up under 'Import As'
 	func get_import_options(preset):
-		var options = []
+		var options = [
+			{'name': 'bone_map', 'default_value': ''}
+		]
 		#options.append( { "name":"Pack in scene", "default_value":false } )
 		#options.append( { "name":"target_path", "default_value":"" } )
 		return options
-	
+
 	#The Number of presets
 	func get_preset_count():
 		return 0
-	
+
 	#The Name of the preset.
 	func get_preset_name(preset):
 		return "Default"
-	
-	#Gets called when pressing a file gets imported / reimported
-	func import( source_path, save_path, options, platforms, gen_files ):
+
+	func vox_arr():
 		#Initialize and populate voxel array
 		var voxelArray = []
 		for x in range(0,128):
@@ -74,120 +149,165 @@ class ImportPlugin extends EditorImportPlugin:
 			for y in range(0,128):
 				voxelArray[x].append([])
 				voxelArray[x][y].resize(128)
-		
+		return voxelArray
+
+	#Gets called when pressing a file gets imported / reimported
+	func import( source_path, save_path, options, platforms, gen_files ):
+
+
 		var file = File.new()
 		var error = file.open( source_path, File.READ )
 		if error != OK:
 			if file.is_open(): file.close()
 			return error
-		
+
 		##################
 		#  Import Voxels #
 		##################
 		var colors = null
-		var data = null
+		var data = []
 		#var derp = PoolByteArray(file.get_8()).get
 		var magic = PoolByteArray([file.get_8(),file.get_8(),file.get_8(),file.get_8()]).get_string_from_ascii()
-		
+
 		var version = file.get_32()
-		 
+
 		# a MagicaVoxel .vox file starts with a 'magic' 4 character 'VOX ' identifier
 		if magic == "VOX ":
 			var sizex = 0
 			var sizey = 0
 			var sizez = 0
-			
+			var names = {}
+			var chunkNum = 0
+			var CHUNK_DBG = false
+
 			while file.get_position() < file.get_len():
 				# each chunk has an ID, size and child chunks
-				var chunkId = PoolByteArray([file.get_8(),file.get_8(),file.get_8(),file.get_8()]).get_string_from_ascii() #char[] chunkId
-				var chunkSize = file.get_32()
-				var childChunks = file.get_32()
-				var chunkName = chunkId
-				# there are only 2 chunks we only care about, and they are SIZE and XYZI
+
+				var chunk = MVChunk.new()
+				chunk.init(file)
+				var chunkName = chunk.id
+				# there are only a few chunks we care about, and they are SIZE, XYZI, TRNG?, RGBA
 				if chunkName == "SIZE":
 					sizex = file.get_32()
 					sizey = file.get_32()
 					sizez = file.get_32()
-					 
-					file.get_buffer(chunkSize - 4 * 3)
+
+					file.get_buffer(chunk.size - 4 * 3)
 				elif chunkName == "XYZI":
 					# XYZI contains n voxels
 					var numVoxels = file.get_32()
-					
+					var chunkData = {'data': [], 'vox': vox_arr()}
+					data.append(chunkData)
 					# each voxel has x, y, z and color index values
-					data = []
 					for i in range(0,numVoxels):
-						var mvc = MagicaVoxelData.new()
+						var mvc = MVVoxels.new()
 						mvc.init(file)
-						data.append(mvc)
-						voxelArray[mvc.pos.x][mvc.pos.y][mvc.pos.z] = mvc
+						mvc.chunkNum = chunkNum
+						chunkData.data.append(mvc)
+						chunkData.vox[mvc.pos.x][mvc.pos.y][mvc.pos.z] = mvc
 				elif chunkName == "RGBA":
 					colors = []
-					 
+
 					for i in range(0,256):
 						var r = float(file.get_8() / 255.0)
 						var g = float(file.get_8() / 255.0)
 						var b = float(file.get_8() / 255.0)
 						var a = float(file.get_8() / 255.0)
-						
+
 						colors.append(Color(r,g,b,a))
-						
-				else: file.get_buffer(chunkSize)  # read any excess bytes
-			
-			if data.size() == 0: return data #failed to read any valid voxel data
-			 
-			# now push the voxel data into our voxel chunk structure
-			for i in range(0,data.size()):
-				# use the voxColors array by default, or overrideColor if it is available
-				if colors == null:
-					data[i].color = to_rgb(voxColors[data[i].color]-1)
+				elif chunkName == "nTRN":
+					var tfmNode = MVTransformNode.new()
+					tfmNode.init(file)
+					#if CHUNK_DBG:
+					print('{0}'.format([tfmNode]).left(1000))
+					if '_name' in tfmNode.attr:
+						names[tfmNode.node_id] = tfmNode.attr._name
 				else:
-					data[i].color = colors[data[i].color-1]
+					var buff = file.get_buffer(chunk.size)  # read any excess bytes
+					if !(chunkName in ['MATL', 'rOBJ']) and chunkName and CHUNK_DBG:
+						print('Unknown chunk: %s with %d children (%db)' % [chunkName, chunk.child_count, chunk.size])
+						print('{0}'.format([buff]))
+				if file.get_position() < file.get_len() && file.get_position() != chunk.position + chunk.size:
+					var diff = chunk.position + chunk.size - file.get_position()
+					if CHUNK_DBG:
+						print('file position is wrong reading %s %d != %d + %d (%d), skip %d bytes' % [chunkName, file.get_position(), chunk.position, chunk.size, chunk.position + chunk.size, diff])
+					var buff = file.get_buffer(diff)
+					if CHUNK_DBG:
+						# buff might have leading 0
+						#buff = buff.subarray(1, buff.size()-1)
+						print('{0} {1}'.format([buff, buff.get_string_from_ascii()]))
+					assert(diff > 0)
+				chunkNum += 1
+			if data.size() == 0: return data #failed to read any valid voxel data
+
+			var bone_map = {}
+			for name_id in options.bone_map.split(','):
+				var parts = name_id.split('=')
+				bone_map[parts[0]] = parts[1]
+			# now push the voxel data into our voxel chunk structure
+			for chunk in data:
+				for i in range(0, chunk.data.size()):
+					var d = chunk.data[i]
+					# use the voxColors array by default, or overrideColor if it is available
+					if colors == null:
+						d.color = to_rgb(voxColors[d.color]-1)
+					else:
+						d.color = colors[d.color-1]
+					var bone_name = names[d.chunkNum]
+					d.bone = bone_map[bone_name]
 		file.close()
-		
+
 		##################
 		#   Create Mesh  #
 		##################
-		
+
 		#Calculate offset
 		var s_x = 1000
 		var m_x = -1000
 		var s_z = 1000
 		var m_z = -1000
-		for voxel in data:
-			if voxel.pos.x < s_x: s_x = voxel.pos.x
-			elif voxel.pos.x > m_x: m_x = voxel.pos.x
-			if voxel.pos.z < s_z: s_z = voxel.pos.z
-			elif voxel.pos.z > m_z: m_z = voxel.pos.z
+		# todo: separate offset per chunk? transform using transformnodes above?
+		for chunk in data:
+			for d in chunk.data:
+				var p = d.pos
+				if p.x < s_x: s_x = p.x
+				elif p.x > m_x: m_x = p.x
+				if p.z < s_z: s_z = p.z
+				elif p.z > m_z: m_z = p.z
 		var x_dif = m_x - s_x
 		var z_dif = m_z - s_z
 		var dif = Vector3(-s_x-x_dif/2.0,0,-s_z-z_dif/2.0)
-		
+
 		#Create the mesh
 		var st = SurfaceTool.new()
 		st.begin(Mesh.PRIMITIVE_POINTS)
-		for voxel in data:
-			var normal = Vector3(0, 0, 0)
-			
-			if not above(voxel,voxelArray): normal += normals.up
-			if not below(voxel,voxelArray): normal += normals.down
-			if not onleft(voxel,voxelArray): normal += normals.left
-			if not onright(voxel,voxelArray): normal += normals.right
-			if not infront(voxel,voxelArray): normal += normals.front
-			if not behind(voxel,voxelArray): normal += normals.back
-			
-			st.add_color(voxel.color)
-			normal = normal.normalized()
-			st.add_normal(normal)
-			print(normal)
-			#st.add_tangent(normal.normalized())
-			st.add_vertex(voxel.pos + dif)
-			"""
-			for tri in to_draw:
-				st.add_vertex( (tri*0.5)+voxel.pos+dif)
-			"""
+		for chunk in data:
+			var voxelData = chunk.vox
+			for voxel in chunk.data:
+				var normal = Vector3(0, 0, 0)
+
+				if not above(voxel,voxelData): normal += normals.up
+				if not below(voxel,voxelData): normal += normals.down
+				if not onleft(voxel,voxelData): normal += normals.left
+				if not onright(voxel,voxelData): normal += normals.right
+				if not infront(voxel,voxelData): normal += normals.front
+				if not behind(voxel,voxelData): normal += normals.back
+
+				st.add_color(voxel.color)
+				normal = normal.normalized()
+				st.add_normal(normal)
+				# todo: add multiple bones? weight painted?
+				if voxel.bone:
+					st.add_bones([voxel.bone, 0, 0, 0])
+					st.add_weights([1, 0, 0, 0])
+				#st.add_tangent(normal.normalized())
+				st.add_vertex(voxel.pos + dif)
+				"""
+				for tri in to_draw:
+					st.add_vertex( (tri*0.5)+voxel.pos+dif)
+				"""
 		#st.generate_normals()
-		
+
 		var shader_path = self.get_script().get_path().replace('plugin.gd', 'points.shader')
 		var material = ShaderMaterial.new()
 		material.shader = shader
@@ -197,17 +317,17 @@ class ImportPlugin extends EditorImportPlugin:
 		#material.set_flag(material.FLAG_USE_COLOR_ARRAY,true)
 		st.set_material(material)
 		var mesh
-		
+
 		if file.file_exists(save_path) and false:
 			var old_mesh = ResourceLoader.load(save_path)
 			old_mesh.surface_remove(0)
 			mesh = st.commit(old_mesh)
 		else:
 			mesh = st.commit()
-		
+
 		var full_path = "%s.%s" % [save_path, get_save_extension()]
 		return ResourceSaver.save( full_path, mesh )
-	
+
 	#Data
 	var voxColors = [
 		0x00000000, 0xffffffff, 0xffccffff, 0xff99ffff, 0xff66ffff, 0xff33ffff, 0xff00ffff, 0xffffccff, 0xffccccff, 0xff99ccff, 0xff66ccff, 0xff33ccff, 0xff00ccff, 0xffff99ff, 0xffcc99ff, 0xff9999ff,
@@ -227,67 +347,67 @@ class ImportPlugin extends EditorImportPlugin:
 		0xff000022, 0xff000011, 0xff00ee00, 0xff00dd00, 0xff00bb00, 0xff00aa00, 0xff008800, 0xff007700, 0xff005500, 0xff004400, 0xff002200, 0xff001100, 0xffee0000, 0xffdd0000, 0xffbb0000, 0xffaa0000,
 		0xff880000, 0xff770000, 0xff550000, 0xff440000, 0xff220000, 0xff110000, 0xffeeeeee, 0xffdddddd, 0xffbbbbbb, 0xffaaaaaa, 0xff888888, 0xff777777, 0xff555555, 0xff444444, 0xff222222, 0xff111111
 		]
-	
+
 	var top = [
 		Vector3( 1.0000, 1.0000, 1.0000),
 		Vector3(-1.0000, 1.0000, 1.0000),
 		Vector3(-1.0000, 1.0000,-1.0000),
-		
+
 		Vector3(-1.0000, 1.0000,-1.0000),
 		Vector3( 1.0000, 1.0000,-1.0000),
 		Vector3( 1.0000, 1.0000, 1.0000),
 	]
-	
+
 	var down = [
 		Vector3(-1.0000,-1.0000,-1.0000),
 		Vector3(-1.0000,-1.0000, 1.0000),
 		Vector3( 1.0000,-1.0000, 1.0000),
-		
+
 		Vector3( 1.0000, -1.0000, 1.0000),
 		Vector3( 1.0000, -1.0000,-1.0000),
 		Vector3(-1.0000, -1.0000,-1.0000),
 	]
-	
+
 	var front = [
 		Vector3(-1.0000, 1.0000, 1.0000),
 		Vector3( 1.0000, 1.0000, 1.0000),
 		Vector3( 1.0000,-1.0000, 1.0000),
-		
+
 		Vector3( 1.0000,-1.0000, 1.0000),
 		Vector3(-1.0000,-1.0000, 1.0000),
 		Vector3(-1.0000, 1.0000, 1.0000),
 	]
-	
+
 	var back = [
 		Vector3( 1.0000,-1.0000,-1.0000),
 		Vector3( 1.0000, 1.0000,-1.0000),
 		Vector3(-1.0000, 1.0000,-1.0000),
-		
+
 		Vector3(-1.0000, 1.0000,-1.0000),
 		Vector3(-1.0000,-1.0000,-1.0000),
 		Vector3( 1.0000,-1.0000,-1.0000)
 	]
-	
+
 	var left = [
 		Vector3(-1.0000, 1.0000, 1.0000),
 		Vector3(-1.0000,-1.0000, 1.0000),
 		Vector3(-1.0000,-1.0000,-1.0000),
-		
+
 		Vector3(-1.0000,-1.0000,-1.0000),
 		Vector3(-1.0000, 1.0000,-1.0000),
 		Vector3(-1.0000, 1.0000, 1.0000),
 	]
-	
+
 	var right = [
 		Vector3( 1.0000, 1.0000, 1.0000),
 		Vector3( 1.0000, 1.0000,-1.0000),
 		Vector3( 1.0000,-1.0000,-1.0000),
-		
+
 		Vector3( 1.0000,-1.0000,-1.0000),
 		Vector3( 1.0000,-1.0000, 1.0000),
 		Vector3( 1.0000, 1.0000, 1.0000),
 	]
-	
+
 	var normals = {
 		'up': Vector3(0, 1, 0),
 		'down': Vector3(0, -1, 0),
@@ -296,7 +416,7 @@ class ImportPlugin extends EditorImportPlugin:
 		'front': Vector3(0, 0, 1),
 		'back': Vector3(0, 0, -1)
 	}
-	
+
 	#Some staic functions
 	func above(cube,array): return array[cube.pos.x][cube.pos.y+1][cube.pos.z]
 	func below(cube,array): return array[cube.pos.x][cube.pos.y-1][cube.pos.z]
