2eae77c932382088369cbb441c6cc4d302543022
Jason Yue <jyue@fortinet.com>
2023-04-26 11:19:36 -0700

    Always set head rendering value and use self keyword

--- addons/MagicaVoxelImporter/VoxelMesh.gd
+++ addons/MagicaVoxelImporter/VoxelMesh.gd
@@ -29,14 +29,13 @@ func _set_neck_height(value):
 			mat.set_shader_param('neck_height', value)
 
 func set_render_head(value):
+	render_head = value
 	if is_inside_tree():
-		render_head = value
-		var vox_char_node := $"." as MeshInstance
-		var mat = vox_char_node.get_surface_material(0)
+		var mat = get_surface_material(0)
 		if !mat:
-			mat = vox_char_node.mesh.surface_get_material(0).duplicate()
+			mat = self.mesh.surface_get_material(0).duplicate()
 			if !Engine.is_editor_hint():
-				vox_char_node.set_surface_material(0, mat)
+				self.set_surface_material(0, mat)
 			if mat.get_shader_param("render_head") == value:
 				return
 		mat.set_shader_param("render_head", value)
