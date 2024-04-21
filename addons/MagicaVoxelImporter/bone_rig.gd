4496b50dfc1317f90845531061c96ea0dc55a9eb
Jamie Pate <jpate@fortinet.com>
2024-04-21 16:29:13 -0700

    Finish conversion to bone_rig4
    Fixes weight blending transition between bones

--- addons/MagicaVoxelImporter/bone_rig.gd
+++ addons/MagicaVoxelImporter/bone_rig.gd
@@ -4,18 +4,62 @@ extends Area3D
 const BoneRig4 = preload("./bone_rig4.gd")
 
 # todo: plugin menu?
+## Click here to upgrade to the new layout
 @export var upgrade_3to4 := false: set = _set_upgrade_3to4
 
+# todo: plugin menu?
+# Keep these values so we can copy them to the new node
+@export var auto_reimport := true
+
+# magic property that will update the prefix on ALL rig children!
+@export var bone_prefix: String
 @export var blend_factor: float = 1.0
+@export_node_path("MeshInstance3D") var mesh_path: NodePath
+@export_node_path("Skeleton3D") var skeleton_path: NodePath
 
 var _new_prefix = null
 var _old_prefix = null
 var _collecting_bones := false
 
 
+func _shape_name_to_bone_name(value: String):
+	var parts = value.rsplit('#', 1)
+	return parts[0] if len(parts) else ''
+
 func _set_upgrade_3to4(value):
 	# Click here to upgrade to the new version
-	if !value || !Engine.is_editor_hint():
+	if !value || !Engine.is_editor_hint() || !is_inside_tree():
 		return
-	var node = Node3D.new()
-	node.script = BoneRig4
+	var upgraded = Node3D.new()
+	upgraded.name = "%s4" % [name]
+	var p := get_parent()
+	upgraded.script = BoneRig4
+	add_sibling(upgraded)
+	upgraded.owner = owner
+	var usage_ok = PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_SCRIPT_VARIABLE
+	var usage_never = PROPERTY_USAGE_NEVER_DUPLICATE
+	for prop in get_property_list():
+		if prop.usage & usage_ok && !prop.usage & usage_never && prop.name in upgraded:
+			upgraded[prop.name] = self[prop.name]
+	var bones := {}
+	for s in get_children():
+		var bone_name = _shape_name_to_bone_name(s.name)
+		if !bone_name in bones:
+			bones[bone_name] = []
+		bones[bone_name].append(s)
+	for bone_name in bones:
+		var area := Area3D.new()
+		area.name = bone_name
+		area.collision_mask = 0
+		area.monitorable = false
+		area.monitoring = false
+		upgraded.add_child(area)
+		area.owner = owner
+		for s in bones[bone_name]:
+			var new_s = s.duplicate()
+			assert(new_s.transform == s.transform)
+			var parts = s.name.split("#", true, 1)
+			new_s.name = parts[1] if len(parts) > 1 else "Shape"
+			area.add_child(new_s)
+			new_s.owner = owner
+	print("Upgraded: %s" % [upgraded])
--- addons/MagicaVoxelImporter/bone_rig4.gd
+++ addons/MagicaVoxelImporter/bone_rig4.gd
@@ -1,14 +1,11 @@
 @tool
-extends Area3D
+extends Node3D
 
 signal _physics_collide_bones()
-
-const BLEND_DBG := true
 
 # todo: plugin menu?
 # in the meantime you can check 'Collect Bones' to manually run the tool script
 @export var collect_bones := false: set = _set_collect_bones
-@export var upgrade_3to4 := false: set = _set_upgrade_3to4
 @export var mirror_ltr := false: set = _set_mirror_ltr
 @export var mirror_rtl := false: set = _set_mirror_rtl
 @export var auto_reimport := true
@@ -20,8 +17,6 @@ const BLEND_DBG := true
 var mesh_path: NodePath: set = _set_mesh_path
 var skeleton_path: NodePath: set = _set_skeleton_path
 
-@export var blend_factor: float = 1.0
-
 var _new_prefix = null
 var _old_prefix = null
 var _collecting_bones := false
@@ -29,11 +24,13 @@ var _collecting_bones := false
 func _ready():
 	set_physics_process(false)
 	if !Engine.is_editor_hint():
-		for c in get_children():
-			if c is CollisionShape3D:
-				c.disabled = true
-	monitoring = false
-	monitorable = false
+		# just remove ourself outside the editor
+		queue_free()
+	for a in get_children():
+		if a is Area3D:
+			a.monitorable = false
+			a.monitoring = false
+
 
 func _get_property_list():
 	var result = [
@@ -57,13 +54,6 @@ func _set_collect_bones(value):
 		_collect_bones_once()
 
 
-func _set_upgrade_3to4(value):
-	if !value || !Engine.is_editor_hint() || self is Area:
-		return
-	var node = Node3D.new()
-	node.script = script_changed
-
-
 func _set_mirror_ltr(value):
 	mirror_ltr = value
 	if value:
@@ -77,13 +67,15 @@ func _set_mirror_rtl(value):
 	mirror_rtl = false
 
 func _mirror_shapes(from: String, to: String):
-	for c in get_children():
-		if c is CollisionShape3D && c.name.find(from) > -1:
-			var mirror_name = c.name.replace(from, to)
-			var other = get_node_or_null(mirror_name)
-			if other:
-				other.transform.origin = c.transform.origin * Vector3(-1, 1, 1)
-				other.transform.basis = c.transform.basis.rotated(Vector3.UP, deg_to_rad(180))
+	for a in get_children():
+		var mirror_name = a.name.replace(from, to)
+		var other = get_node_or_null(mirror_name)
+		for c in a.get_children():
+			if c is CollisionShape3D && a.name.find(from) > -1:
+				var other_c := other.get_node_or_null(NodePath(c.name)) if other else null
+				if other_c:
+					other_c.global_transform.origin = c.global_transform.origin * Vector3(-1, 1, 1)
+					other_c.global_transform.basis = c.global_transform.basis.rotated(Vector3.UP, deg_to_rad(180))
 
 func _set_bone_prefix(value):
 	if value == bone_prefix:
@@ -112,10 +104,6 @@ func _set_skeleton_path(value: NodePath):
 	_collect_bones_once()
 
 
-func _shape_name_to_bone_name(value: String):
-	var parts = value.rsplit('#', 1)
-	return parts[0] if len(parts) else ''
-
 func _collect_bones_once():
 	if is_inside_tree() && !collect_bones && Engine.is_editor_hint():
 		if !self in EditorInterface.get_selection().get_selected_nodes():
@@ -138,21 +126,18 @@ func _collect_bones():
 	var start := Time.get_ticks_msec()
 	var skel := get_node_or_null(skeleton_path) as Skeleton3D
 	var mi := get_node_or_null(mesh_path) as MeshInstance3D
-	var area := get_node_or_null(area_path) as Area3D
 	var self_area = self
 	var path = owner.get_parent().get_path_to(self) if owner && owner.get_parent() else get_path()
-	if !area && self_area is Area3D:
-		area = self_area
 	if !mi || !skel:
 		if is_inside_tree():
 			if mesh_path && !mi:
 				printerr('%s Mesh not found at path %s' % [path, mesh_path])
 			if skeleton_path && !skel:
 				printerr('%s Skeleton3D not found at path %s' % [path, skeleton_path])
-			if area_path && !area:
-				printerr('%s Area3D not found at path %s' % [path, area_path])
 			print('%s needs mesh_path:(%s) and skeleton_path:(%s)' % [path, !!mesh_path, !!skeleton_path])
 		return
+	if get_child_count() == 0:
+		printerr("No child areas found")
 	var mesh := mi.mesh as ArrayMesh
 	if !mesh:
 		printerr('%s Mesh %s is not an arraymesh' % [path, mesh])
@@ -163,7 +148,6 @@ func _collect_bones():
 	if mesh.get_surface_count() < 1:
 		printerr('%s Mesh %s has no surface' % [path, mesh])
 		return
-	# TODO: support additional surfaces?
 
 	var s := 0
 	var surface := mesh.surface_get_arrays(s)
@@ -185,8 +169,16 @@ func _collect_bones():
 	assert(weights_sz == 4, "ARRAY_FLAG_USE_8_BONE_WEIGHTS is not supported")
 	bones.resize(vert_count * weights_sz)
 	weights.resize(vert_count * weights_sz)
-	var space := PhysicsServer3D.area_get_space(area.get_rid())
+	var area1 = get_children()[0]
+	# we can just grab state from the first area, all other areas should share the same space
+	var space := PhysicsServer3D.area_get_space(area1.get_rid())
 	var state := PhysicsServer3D.space_get_direct_state(space)
+	if area1.get_child_count() > 1:
+		var area2 := get_children()[1]
+		var space2 := PhysicsServer3D.area_get_space(area2.get_rid())
+		assert(space2 == space)
+		var state2 := PhysicsServer3D.space_get_direct_state(space2)
+		assert(state2 == state2)
 	var USE_BOX = false
 	var voxel = PhysicsServer3D.box_shape_create() if USE_BOX else PhysicsServer3D.sphere_shape_create()
 
@@ -205,34 +197,30 @@ func _collect_bones():
 	qp.collide_with_areas = true
 	qp.collide_with_bodies = false
 	var missing_voxels := []
-	var bone_map = {}
-	var bone_overlap = {}
+	var bone_counts := {}
+	var bone_overlap := {}
 
 	var w := PackedFloat32Array()
 	for i in range(weights_sz):
 		w.append(0.0)
-	var enabled_shapes := {}
-	for c in area.get_children():
-		if c is CollisionShape3D && !c.disabled:
-			var bone_name = _shape_name_to_bone_name(c.name)
-			if !bone_name in enabled_shapes:
-				enabled_shapes[bone_name] = []
-			enabled_shapes[bone_name].append(c)
-	assert(area.collision_mask == 0)
+	for a in get_children():
+		assert(a is Area3D, "All children must be Area3D")
+		assert(a.collision_mask == 0)
+		a.collision_layer = 1
+		for c in a.get_children():
+			assert(c is CollisionShape3D, "All grandchildren must be CollisionShape3D")
 	var fail := []
-	area.collision_layer = 1
 	for i in len(vertices):
 		var v := vertices[i]
 
 		qp.transform = mi.global_transform * Transform3D(Basis(), v)
 		var collisions := state.intersect_shape(qp, weights_sz)
-		var weight_total := 0.0
 		var c_len = len(collisions)
 		if !c_len:
 			missing_voxels.append(qp.transform.origin)
 
 		var bone_count := 0
-		var bone_names := []
+		var collided_bones := []
 		var b_i = weights_sz * i
 		var b := 0
 		while b < weights_sz:
@@ -240,66 +228,76 @@ func _collect_bones():
 			var found := false
 			if b < c_len && b_i + b < vert_count * weights_sz:
 				var shape_idx := collisions[b].shape as int
-				var collider = collisions[b].collider
-				if collider != area:
+				var bone_area = collisions[b].collider
+				if bone_area.get_parent() != self:
 					printerr('Collided %s with something else? %s' % [
 						qp.transform.origin,
-						collider
+						bone_area.get_path()
 					])
 				else:
-					var shape = area.shape_owner_get_owner(area.shape_find_owner(shape_idx))
-					var bone_name = _shape_name_to_bone_name(shape.name)
-					# if multiple shapes capture the same voxel, use the first one in tree order
-					if bone_name in bone_names:
+					# if multiple shapes in the same area capture the same voxel,
+					# use the first collision in tree order
+					var bone_name = bone_area.name
+					if bone_name in collided_bones:
 						collisions.erase(collisions[b])
 						c_len = len(collisions)
 						continue
-					if c_len == 1:
-						w[b] = 1.0
-					else:
-						var weight = _calc_bone_weight(area, enabled_shapes, shape, state, qp.transform.origin)
-						if weight:
-							w[b] = weight
-					weight_total += w[b]
-					if !bone_name in bone_map:
-						bone_map[bone_name] = 0
-					bone_names.append(bone_name)
-					bone_map[bone_name] += 1
-					var bone := skel.find_bone(bone_name)
-					if bone == -1:
-						fail.append('Unable to find bone %s' % [bone_name])
-						printerr()
 					found = true
 					bone_count += 1
+					var skel_bone_name = "%s%s" % [bone_prefix, bone_name]
+					var bone := skel.find_bone(skel_bone_name)
+					if bone == -1:
+						fail.append('Unable to find bone %s' % [skel_bone_name])
 					bones[b_i + b] = bone
-					# TODO: distribute weights?
+					if !bone_name in bone_counts:
+						bone_counts[bone_name] = 0
+					collided_bones.append(bone_name)
+					bone_counts[bone_name] += 1
 			if !found:
 				bones[b_i + b] = 0
+			b += 1
+		if len(collisions) > weights_sz:
+			collisions.resize(weights_sz)
+		if len(collisions) == 1:
+			for w_i in len(w):
+				w[w_i] = 1.0 if w_i == 0 else 0.0
+		else:
+			var area_w_map := {}
+			for c_i in len(collisions):
+				area_w_map[collisions[c_i].collider] = c_i
+			for pair in _pairs(collisions):
+				var p_w := _calc_bone_weights(state, pair[0].collider, pair[1].collider, qp.transform.origin)
+				for p_i in [0, 1]:
+					b = area_w_map[pair[p_i].collider]
+					w[b] = p_w[p_i]
+		var total_check := 0.0
+		var weight_total := 0.0
+		b = 0
+		while b < weights_sz:
+			weight_total += w[b]
 			b += 1
 		b = 0
 		while b < weights_sz:
 			var bw = w[b] / weight_total if weight_total > 0 else 0.0
-			if blend_factor != 1.0:
-				bw = max(0.0, min(1.0, (bw - 0.5) * blend_factor + 0.5))
-			if bw < 0.9999 && bw > 0.0001:
-				print("blendfactor: ", bw, " ", b, " ", qp.transform.origin)
 			weights[b_i + b] = bw
+			total_check += weights[b_i + b]
 			b += 1
-		if len(bone_names) > 1:
-			bone_names.sort()
-			if !bone_names in bone_overlap:
-				bone_overlap[bone_names] = 0
-			bone_overlap[bone_names] += 1
+		if len(collided_bones) > 1:
+			collided_bones.sort()
 
-	area.collision_layer = 0
+			if !collided_bones in bone_overlap:
+				bone_overlap[collided_bones] = 0
+			bone_overlap[collided_bones] += 1
+	for a in get_children():
+		a.collision_layer = 0
 	PhysicsServer3D.free_rid(voxel)
 	if fail:
 		printerr('Fatal error: %s' % ['\n'.join(fail.slice(0, 10))])
 		return
 	var bone_ids = {}
-	for bone in bone_map:
-		bone_ids[bone] = skel.find_bone(bone)
-	print('\noverlap:\n%s\n\nbones:\n%s\n\nids:\n%s\n' % [bone_overlap, bone_map, bone_ids])
+	for bone in bone_counts:
+		bone_ids[bone] = skel.find_bone("%s%s" % [bone_prefix, bone])
+	print('\noverlap:\n%s\n\nbones:\n%s\n\nids:\n%s\n' % [bone_overlap, bone_counts, bone_ids])
 	if len(missing_voxels) == len(vertices):
 		printerr('No collisions found!')
 		return
@@ -332,32 +330,64 @@ func _collect_bones():
 		call_deferred('re_import')
 
 
-func _calc_bone_weight(area: Area3D, enabled_shapes: Dictionary, shape: CollisionShape3D, state: PhysicsDirectSpaceState3D, origin: Vector3) -> float:
+# Return each unique pair of items from all items of an array
+func _pairs(array):
+	var result := []
+	for i in range(len(array)):
+		for j in range(i + 1, len(array)):
+			result.append([array[i], array[j]])
+	return result
+
+
+func _get_area_center(area: Area3D) -> Vector3:
+	# be lazy and just take the average center of all shapes as the 'center'
+	# to choose the ray direction
+	var aabb = null
+	for s in area.get_children():
+		if !s.disabled:
+			var s_origin: Vector3 = s.global_transform.origin
+			aabb = aabb.expand(s_origin) if aabb else AABB(s_origin, Vector3())
+	assert(aabb, "Area has no children? %s" % [area.get_path()])
+	return aabb.get_center()
+
+
+func _ray_into(state: PhysicsDirectSpaceState3D, area: Area3D, ray_direction: Vector3, to_voxel: Vector3):
+	var exclude := []
+	for a in get_children():
+		if a != area:
+			exclude.append((a as Area3D).get_rid())
+	var ray_len := 10000.0
+	var ray_end := to_voxel
+
+	var ray_start := ray_end + ray_direction * -ray_len
+	var ray_params := PhysicsRayQueryParameters3D.create(ray_start, ray_end, ~0, [])
+	ray_params.collide_with_bodies = false
+	ray_params.collide_with_areas = true
+	ray_params.exclude = exclude
+	var intersect = state.intersect_ray(ray_params)
+	if intersect:
+		return intersect.position
+	else:
+		# assume we want 0 weight if we don't collide (on the edge)
+		return null
+
+
+func _calc_bone_weights(state: PhysicsDirectSpaceState3D, a1: Area3D, a2: Area3D, origin: Vector3) -> Array[float]:
 	"""
 	Get the unnormalized bone weight for a bone based on the distance to the edge of the shape.
 	Calculates the ray for each shape pair that overlaps for this voxel
 	and sum the distances along that ray to the outside of the shape for each shape?
 	"""
 	var RAY_LEN = 1000.0
-	var bone_name = _shape_name_to_bone_name(shape.name)
-	print("exclude: ", bone_name, exclude)
-	print("notexclude: ", enabled_shapes[bone_name][0].get_rid())
-	var ray_start = shape.global_transform.origin
-	var ray_end = ray_start + ray.normalized() * RAY_LEN
-	# ray points outward from the center of the shape from the current voxel
-	var ray = ray_end - origin
-	var ray_start = ray_end - ray.normalized() * RAY_LEN
-	var ray_param = PhysicsRayQueryParameters3D.create(ray_start, ray_end, ~0, [])
-	ray_param.collide_with_bodies = false
-	ray_param.collide_with_areas = true
-	ray_param.exclude = exclude
-	var intersect = state.intersect_ray(ray_param)
-	if intersect:
-		printerr("intersect: ", intersect, " ", shape.shape.get_rid() == intersect.rid)
-		return intersect.position.distance_to(origin)
-	else:
-		printerr("intersection didn't work with %s %s > %s" % [shape.name, ray_start, ray_end])
-		return 0.0
+	var bone1_name = a1.name
+	var bone2_name = a2.name
+	var exclude = []
+	var c1 := _get_area_center(a1)
+	var c2 := _get_area_center(a2)
+	var p1 = _ray_into(state, a1, (c1 - c2).normalized(), origin)
+	var p2 = _ray_into(state, a2, (c2 - c1).normalized(), origin)
+	return [p1.distance_to(origin) if p1 else 0, p2.distance_to(origin) if p2 else 0]
+
 
 func re_import():
 	if Engine.is_editor_hint():
