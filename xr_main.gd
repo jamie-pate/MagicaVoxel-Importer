ac95e6d07ee6dedb05bb6fc24474795f4100dbf2
Jamie Pate <jpate@fortinet.com>
2025-01-10 11:22:57 -0800

    Enable Native Mobile XR

--- xr_main.gd
+++ xr_main.gd
@@ -3,7 +3,15 @@ extends Node3D
 func _ready():
 	var interface := XRServer.find_interface("OpenXR")
 	if !interface.is_initialized():
-		interface.initialize()
+		if interface.initialize():
+			print("Initialized OpenXR")
+		else:
+			interface = XRServer.find_interface("Native mobile")
+			if interface && !interface.is_initialized() && interface.initialize():
+				print("Initialized Native Mobile XR")
+			else:
+				printerr("Failed to initialize XR")
+
 		#interface.start_passthrough()
 	get_viewport().transparent_bg = true
 	get_viewport().use_xr = true
